1cccc487fa38c8d90ab730dd0195136757d7ad80
7Wanderer <khanahyan.022@gmail.com>
2023-11-16 19:28:28 +0000

    Started on panels

--- src/SwingWindow.java
+++ src/SwingWindow.java
@@ -1,9 +1,18 @@
 package src;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.*;
-import java.util.Stack;
 
 public class SwingWindow extends JFrame {
+
+    JMenuBar menubar = new JMenuBar();
+    JMenu menu = new JMenu("Menu");
+    JMenuItem register = new JMenuItem("Register");
+    JMenuItem login = new JMenuItem("Login");
+
+    JPanel panel;
+    JPanel registerPanel;
 
     public SwingWindow(String text) {
         super(text);
@@ -11,12 +20,7 @@ public class SwingWindow extends JFrame {
         Dimension screenSize = toolkit.getScreenSize();
         setSize(screenSize.width/2, screenSize.height/2);
         setLocation(screenSize.width/4, screenSize.height/4);
-        System.out.println(screenSize.width);
-
-        JMenuBar menubar = new JMenuBar();
-        JMenu menu = new JMenu("Menu");
-        JMenuItem register = new JMenuItem("Register");
-        JMenuItem login = new JMenuItem("Login");
+        
         menu.add(register);
         menu.add(login);
         menubar.add(menu);
@@ -28,19 +32,38 @@ public class SwingWindow extends JFrame {
          * I'll also upgrade the JPanel when necessary.
          */
 
-        JPanel panel = new JPanel();
+        panel = new JPanel();
         panel.setLayout(new GridLayout(5, 4));
 
         addButton(panel, "Button1");
 
+        registerPanel = new RegisterLoginPanels();
 
+        this.add(registerPanel, BorderLayout.CENTER);
+        registerPanel.setVisible(false);
         this.add(panel, BorderLayout.CENTER);
+        this.addListeners();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
     private void addButton(JPanel panel, String label) {
         JButton button = new JButton(label);
         button.setFont(new Font("Monospaced", Font.BOLD, 22));
+        
         panel.add(button);
+    }
+    public void addListeners() {
+    	register.addActionListener(new ActionListener() {
+    		public void actionPerformed(ActionEvent e) {
+    			System.out.println("Plz work register");
+                panel.setVisible(false);
+                registerPanel.setVisible(true);
+    		}
+        });
+        login.addActionListener(new ActionListener() {
+    		public void actionPerformed(ActionEvent e) {
+    			System.out.println("Plz work login!");
+    		}
+        });
     }
 }
