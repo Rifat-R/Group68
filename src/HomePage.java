2637bd5697d09a04e62e4e38797910b50ab086ef
stormfury51 <mgflagz@gmail.com>
2023-11-27 14:17:57 +0000

    Base layout for product table

--- src/HomePage.java
+++ src/HomePage.java
@@ -6,6 +6,7 @@ import javax.swing.table.TableModel;
 import src.User.Role;
 
 import java.sql.*;
+import java.util.concurrent.Flow;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.*;
@@ -17,9 +18,9 @@ public class HomePage extends JPanel {
 
     // Only for registered users
     public HomePage() {
-        //this.setLayout(new GridBagLayout());
-        // GridBagConstraints c = new GridBagConstraints();
 
+        this.setLayout(new GridBagLayout());
+        /*
         DefaultTableModel model = new DefaultTableModel();
         JTable table = new JTable(model);
         table.setDefaultEditor(Object.class, null);
@@ -31,6 +32,14 @@ public class HomePage extends JPanel {
         model.addColumn("Gauge"); 
         model.addColumn("Era"); 
         model.addColumn("dCCCode");
+        */
+        GridBagConstraints c = new GridBagConstraints();
+
+        Box container = Box.createVerticalBox();
+        
+
+        JPanel individualProduct = new JPanel();
+        individualProduct.setLayout(new GridBagLayout());
 
 
         db = new EasyDatabase();
@@ -43,14 +52,87 @@ public class HomePage extends JPanel {
                 String gauge = rs.getString(5);
                 String era = rs.getString(6);
                 String dcc = rs.getString(7);
-                model.addRow(new Object[]{name, brand, price, gauge, era, dcc});
+                //model.addRow(new Object[]{name, brand, price, gauge, era, dcc});
+
+                JPanel f = new JPanel();
+                f.setLayout(new GridBagLayout());
+
+                JLabel label1 = new JLabel(name);
+                label1.setText(name);
+                c.anchor = GridBagConstraints.FIRST_LINE_START;
+                c.fill = GridBagConstraints.HORIZONTAL;
+                c.gridx = 0;
+                c.gridy = 0;
+                c.gridwidth = 1;
+                c.insets = new Insets(0,0,0,0);
+                f.add(label1, c);
+
+                JLabel label2 = new JLabel(brand);
+                label2.setText(brand);
+                c.fill = GridBagConstraints.HORIZONTAL;
+                c.gridx = 0;
+                c.gridy = 1;
+                c.gridwidth = 1;
+                f.add(label2, c);
+
+                JLabel label3 = new JLabel(price);
+                label3.setText(price);
+                c.fill = GridBagConstraints.HORIZONTAL;
+                c.gridx = 1;
+                c.gridy = 1;
+                c.gridwidth = 1;
+                f.add(label3, c);
+
+                
+                JButton bag = new JButton("Add");
+                c.fill = GridBagConstraints.VERTICAL;
+                c.gridx = 2;
+                c.gridy = 0;
+                c.gridheight = 2;
+                c.gridwidth = 1;
+                c.insets = new Insets(0,30,0,0);
+                f.add(bag, c);
+
+                SpinnerModel model = new SpinnerNumberModel(1, 1, 100, 1);     
+                JSpinner spinner = new JSpinner(model);
+                c.fill = GridBagConstraints.VERTICAL;
+                c.gridx = 3;
+                c.gridy = 0;
+                c.gridheight = 2;
+                c.gridwidth = 1;
+                c.insets = new Insets(0,30,0,10);
+                f.add(spinner, c);
+                
+
+                c.anchor = GridBagConstraints.FIRST_LINE_START;
+                c.fill = GridBagConstraints.VERTICAL;
+                c.gridx = 0;
+                c.gridy = GridBagConstraints.RELATIVE;
+                c.gridheight = 1;
+                c.gridwidth = 1;
+                c.insets = new Insets(0,0,25,0);
+                individualProduct.add(f, c);
+                individualProduct.validate();
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        
-        JScrollPane scrollPane = new JScrollPane(table);
 
-        this.add(scrollPane);   
+        individualProduct.validate();
+        
+        
+
+        JScrollPane scrollPane = new JScrollPane(individualProduct);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.getViewport().setMinimumSize(new Dimension(450, 500));
+        scrollPane.getViewport().setPreferredSize(new Dimension(450, 500));
+
+        c.anchor = GridBagConstraints.PAGE_START;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(0,0,40,0);
+        this.add(scrollPane, c);
+
     }
 }
